f4497cd7dd16764491ed240765622a079be68e71
cy <403325714@qq.com>
2016-05-12 23:28:52 +0800

    initial code

---
+++ server/public/type_def/stock.py
@@ -0,0 +1,19 @@
+class StockInfo(object):
+    """股票的基本信息
+
+    Attributes:
+        code:
+        name:
+    """
+
+    def __init__(self, code):
+        """构造函数"""
+        self.code = code
+        self.name = "unknown"
+
+
+def main():
+    pass
+
+if __name__ == '__main__':
+    main()
---
+++ server/stock_data/update_data.py
@@ -0,0 +1,12 @@
+#!usr/bin/python
+# Filename: update_data.py
+import json
+
+
+def main():
+    pass
+
+if __name__ == "__main__":
+    main()
+
+# End of update_data.py
